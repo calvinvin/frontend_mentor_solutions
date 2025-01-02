6bbbbfd1cd6d99a16c2d6f0db9ca253cc3e5f95e
Calvin <calvinvin@gmail.com>
2025-01-02 16:10:33 +0800

    v1.8.5 on 2025-01-02. Update '4-4 news-homepage-main', close side navigation by click outside.

--- 4-4 news-homepage-main/main.js
+++ 4-4 news-homepage-main/main.js
@@ -1,6 +1,7 @@
 const openSideNavButton = document.getElementById("hamburger-icon");
 const closeSideNavButton = document.getElementById("close-icon");
 const primaryNavigation = document.getElementById("primary-navigation");
+const outsideOfSideNav = document.getElementById("background-blurrer");
 
 function toggleSideNav() {
     primaryNavigation.classList.toggle("show-side-nav");
@@ -8,3 +9,4 @@ function toggleSideNav() {
 
 openSideNavButton.addEventListener("click", toggleSideNav);
 closeSideNavButton.addEventListener("click", toggleSideNav);
+outsideOfSideNav.addEventListener("click", toggleSideNav);
