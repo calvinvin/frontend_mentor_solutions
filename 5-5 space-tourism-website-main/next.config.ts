128b7adbd8a00ac8ac57d97997623897e15c3a07
Calvin <calvinvin@gmail.com>
2025-01-29 23:44:14 +0800

    .

--- 5-5 space-tourism-website-main/next.config.ts
+++ 5-5 space-tourism-website-main/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/frontend_mentor_solutions-main/5-5%20space-tourism-website-main",
 };
 
 export default nextConfig;
