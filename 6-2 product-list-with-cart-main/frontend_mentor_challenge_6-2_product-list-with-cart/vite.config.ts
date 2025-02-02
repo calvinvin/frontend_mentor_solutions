1f72975d92a1db8ee18b85781cbdcf81825ee339
Calvin <calvinvin@gmail.com>
2025-02-02 18:17:17 +0800

    v1.16.2 on 2025-02-02. Build '6-2 product-list-with-cart-main'.

--- 6-2 product-list-with-cart-main/frontend_mentor_challenge_6-2_product-list-with-cart/vite.config.ts
+++ 6-2 product-list-with-cart-main/frontend_mentor_challenge_6-2_product-list-with-cart/vite.config.ts
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/frontend_mentor_solutions/6-2 product-list-with-cart-main/dist/',
+  publicDir: '/frontend_mentor_solutions/6-2 product-list-with-cart-main/dist/public',
 })
