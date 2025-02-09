5f4d790dda9da43ae4b07a30386ec45e9a5933fb
Calvin <calvinvin@gmail.com>
2025-02-09 21:51:06 +0800

    v1.17.3 on 2025-02-09. Build '6-3 mortgage-repayment-calculator-main'.

--- 6-3 mortgage-repayment-calculator-main/frontend_mentor_challenge_6-3_mortgage-repayment-calculator-main/vite.config.ts
+++ 6-3 mortgage-repayment-calculator-main/frontend_mentor_challenge_6-3_mortgage-repayment-calculator-main/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend_mentor_solutions/6-2 product-list-with-cart-main/dist/',
 })
