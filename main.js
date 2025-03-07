42de45c9eac4d8cbf01ad1951745d23a13f9b2c8
Calvin <calvinvin@gmail.com>
2025-03-07 23:32:47 +0800

    .

--- main.js
+++ main.js
@@ -1,40 +1,95 @@
-const content = {};
-// {1: {name, solutions: []} }
-const learningpathHeadingElements = document.querySelectorAll("h2.learning-path__heading");
-learningpathHeadingElements.forEach((learningpathHeadingElement, index)=>{
-    const pathname = learningpathHeadingElement.textContent.split(":")[1].slice(1);
-    learningpathHeadingElement.id = pathname.replaceAll(' ', '-');
-    content[index+1] = {pathname: pathname, solutionList: []};
-})
-const solutionHeadingElements = document.querySelectorAll("h3.solution__heading");
-solutionHeadingElements.forEach(solutionHeadingElement=>{
-    const textContent = solutionHeadingElement.textContent;
-    solutionHeadingElement.id = textContent.slice(3).replaceAll(' ', '-');
-    const path = textContent.charAt(0);
-    content[path].solutionList.push(textContent.slice(3).replaceAll(' ', '-'));
-})
+const createElement = (element) => document.createElement(element);
+const setAttribute = (attribute, value) => (element) => {
+  element[attribute] = value;
+  return element;
+};
+const setHrefAttribute = (value) => (element) =>
+  setAttribute("href", value)(element);
+const addClass = (className) => (element) => {
+  element.classList.add(className);
+  return element;
+};
+const setTextContent = (value) => (element) => {
+  element["textContent"] = value;
+  return element;
+};
+const setId = (id) => (element) => {
+  element.id = id;
+  return element;
+};
+const pipeline = ([headFn, ...tailFns]) =>
+  headFn === undefined
+    ? (input) => input
+    : tailFns.length === 0
+    ? (input) => headFn(input)
+    : (input) => pipeline(tailFns)(headFn(input));
+const appendToElement = (parentElement) => (childElement) => {
+  parentElement.append(childElement);
+  return parentElement;
+};
+const stripSpaceToDash = (str) => str.replaceAll(" ", "-");
+
+const makeElement = (object) => {
+  if (!Object.hasOwn(object, "tagName")) return null;
+  return Object.entries(object).reduce((element, currentEntry) => {
+    const [attribute, value] = currentEntry;
+    return attribute === "tagName"
+      ? element
+      : setAttribute(attribute, value)(element);
+  }, createElement(object.tagName));
+};
+
+const makeTableOfContentElement = (nodeList) => {
+  return Array.from(nodeList).reduce((olElement, currentElement) => {
+    if (currentElement.tagName === "H2") {
+      const aElement = makeElement({
+        tagName: "a",
+        textContent: currentElement.textContent,
+        href: `#${stripSpaceToDash(
+          currentElement.querySelector("span.chapter-name").textContent
+        )}`,
+      });
+      pipeline([
+        appendToElement(createElement("li")),
+        appendToElement(olElement),
+      ])(aElement);
+      return olElement;
+    }
+    if (currentElement.tagName === "H3") {
+      if (olElement.lastElementChild.lastElementChild.tagName !== "OL") {
+        olElement.lastElementChild.append(createElement("ol"));
+      }
+      const aElement = makeElement({
+        tagName: "a",
+        textContent:
+          currentElement.textContent.slice(0, 3) +
+          " " +
+          currentElement.textContent.slice(3),
+        href: `#${stripSpaceToDash(
+          currentElement.querySelector("span.section-name").textContent
+        )}`,
+      });
+      pipeline([
+        appendToElement(createElement("li")),
+        appendToElement(olElement.lastElementChild.lastElementChild),
+      ])(aElement);
+      return olElement;
+    }
+    return olElement;
+  }, createElement("ol"));
+};
 
 const contentElement = document.getElementById("table-of-contents");
-const contentH2Element = document.createElement("h2");
-contentH2Element.textContent = "Table of Contents";
-contentElement.appendChild(contentH2Element);
-const contentOlElement = document.createElement("ol");
-for (let pathIndex of Object.keys(content).sort()) {
-    const contentLiElement = document.createElement("li");
-    const contentLiAElement = document.createElement("a");
-    contentLiAElement.textContent = `Learning Path ${pathIndex}: ${content[pathIndex].pathname}`;
-    contentLiAElement.href = `#${content[pathIndex].pathname.replaceAll(' ', '-')}`;
-    contentLiElement.appendChild(contentLiAElement);
-    const pathOlElement = document.createElement("ol");
-    content[pathIndex].solutionList.forEach((solution, solutionIndex)=>{
-        const solutionLiElement = document.createElement("li");
-        const solutionLiAElement = document.createElement("a");
-        solutionLiAElement.textContent = `${pathIndex}.${solutionIndex+1} ${solution}`;
-        solutionLiAElement.href = `#${solution.replaceAll(' ', '-')}`;
-        solutionLiElement.appendChild(solutionLiAElement);
-        pathOlElement.appendChild(solutionLiElement);
-    })
-    contentLiElement.appendChild(pathOlElement);
-    contentOlElement.appendChild(contentLiElement);
-};
-contentElement.appendChild(contentOlElement);
+contentElement.append(
+  makeTableOfContentElement(
+    document.querySelectorAll("h2.learning-path__heading, h3.solution__heading")
+  )
+);
+document
+  .querySelectorAll("h2.learning-path__heading, h3.solution__heading")
+  .forEach((headingNode) => {
+    setAttribute(
+      "id",
+      stripSpaceToDash(headingNode.querySelectorAll("span")[1].textContent)
+    )(headingNode);
+  });
